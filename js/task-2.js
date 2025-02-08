ac06546c4060c2943bc1d6b4e1af22ce9109bbcd
Sasha <malinka15117kk@gmail.com>
2025-02-08 15:19:50 +0200

    hm6

---
+++ js/task-2.js
@@ -0,0 +1,65 @@
+// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
+// Оголоси наступні методи класу:
+// getItems() — повертає масив поточних товарів у приватній властивості items.
+// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+// Оголошений клас Storage
+// У класі Storage оголошений метод getItems
+// У класі Storage оголошений метод addItem
+// У класі Storage оголошений метод removeItem
+// Властивість items у класі Storage оголошена приватною
+// Метод getItems повертає значення приватної властивості items екземпляра класу, який його викликає
+// Метод addItem змінює значення приватної властивості items екземпляра класу, який його викликає
+// Метод removeItem змінює значення приватної властивості items екземпляра класу, який його викликає
+// У результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення змінної storage — це об'єкт
+// У об’єкта storage немає публічної властивості items
+// Перший виклик storage.getItems() одразу після ініціалізації екземпляра повертає масив ["Nanitoids", "Prolonger", "Antigravitator"]
+// Другий виклик storage.getItems() після виклику storage.addItem("Droid") повертає масив ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// Третій виклик storage.getItems() після виклику storage.removeItem("Prolonger") повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+// Четвертий виклик storage.getItems() після виклику storage.removeItem("Scaner") повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+
+class Storage {
+    #items;
+
+    constructor(initialItems = []) {
+        this.#items = initialItems;
+    }
+    
+    getItems() {
+        return [...this.#items];
+    }
+
+
+    addItem(newItem) {
+        if (!this.#items.includes(newItem)) {
+            this.#items.push(newItem);
+        }
+    }
+
+
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
+
+
+
+
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
